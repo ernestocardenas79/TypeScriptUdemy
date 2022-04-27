ac327a72870eac3ecb790aeed31e7c312b676c81
Ernesto Cardenas Llanes <ernesto.llanes@neoris.com>
2022-04-27 10:34:22 -0600

    ajuste en webpack entry point

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode:'development',
-    entry: path.resolve(__dirname, '/src/app.ts'),
+    entry: path.resolve(__dirname, 'src/app.ts'),
     output:{
         filename: 'bundle.js',
         path:  path.resolve(__dirname,'dist'),
